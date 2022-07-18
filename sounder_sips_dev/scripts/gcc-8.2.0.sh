58b229cfa735f00500bde4c9be013742956f1512
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-07-18 13:14:13 -0700

    Patch GCC 8.2.0 install for install under Ubuntu 20.04

--- sounder_sips_dev/scripts/gcc-8.2.0.sh
+++ sounder_sips_dev/scripts/gcc-8.2.0.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 tar zxf $SOURCES_PATH/gcc-8.2.0.tar.gz
 cd gcc-8.2.0
+# See: https://stackoverflow.com/questions/62435946/building-gcc-8-3-makefile955-all-error-2
+patch -p1 <$SOURCES_PATH/GCCcore-9.2.0-fix-glibc-2.31-libsanitizer.patch
 ./configure \
     --enable-languages=c,c++,fortran \
     --disable-multilib \
